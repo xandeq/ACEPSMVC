5405bb16c9782ff7b93a0158ac0e1d04d93262cb
xandeq <xandeq@gmail.com>
2021-04-05 06:41:01 -0300

    Implementando o IREPOSITORY

---
+++ ACEPSMVC.DataAccess/Data/Repository/IRespository/ICategoriaRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACEPSMVC.DataAccess.Data.Repository.IRespository
+{
+    interface ICategoriaRepository
+    {
+    }
+}
---
+++ ACEPSMVC.Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ACEPSMVC.Models
+{
+    class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Nome da Categoria")]
+        public string Nome { get; set; }
+
+        public int OrdemVisualizacao { get; set; }
+    }
+}
