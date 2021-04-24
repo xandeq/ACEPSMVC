6d093198009c10b2eceb5ee9768d79cefcd1a067
xandeq <xandeq@gmail.com>
2021-04-24 17:59:35 -0300

    Formulario de Contato

---
+++ ACEPSMVC/Models/FormularioDeContato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACEPSMVC.Models
+{
+    public class FormularioDeContato
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
