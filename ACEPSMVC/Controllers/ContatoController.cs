7445f88a1e4eb66f5a5e8e62adcba03f6914cacb
xandeq <xandeq@gmail.com>
2021-04-25 09:21:25 -0300

    Destaques Laterais com Formulario de Contato

---
+++ ACEPSMVC/Controllers/ContatoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using ACEPSMVC.classes;
+
+namespace ACEPSMVC.Models
+{
+    public class ConfigurationSMTP
+    {
+        //SMTP parameters
+        public static string smtpAdress = "smtp.mail.yahoo.com";
+        public static int portNumber = 587;
+        public static bool enableSSL = true;
+        //need it for the secured connection
+        public static string from = "sender email";
+        public static string password = "password of the above email";
+    }
+
+    public class ContatoController : Controller
+    {
+        public IActionResult Index(int tipo = 0)
+        {
+            Enumeradores.TiposContato tipoContato = (Enumeradores.TiposContato)tipo;
+            ViewData["TipoContato"] = tipoContato.ToString();
+
+            return View();
+        }
+
+        public ActionResult EnviarMensagem()
+        {
+            try
+            {
+                Response.StatusCode = 200;
+            }
+            catch (Exception ex)
+            {
+                //Error response
+                Response.StatusCode = 400;
+            }
+            return null;
+        }
+    }
+}
---
+++ ACEPSMVC/classes/Enumeradores.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACEPSMVC.classes
+{
+    public class Enumeradores
+    {
+        public enum TiposContato
+        {
+            Contato = 0,
+            Reclamacao = 1,
+            Sugestao = 2,
+            Elogio = 3,
+        }
+    }
+}
