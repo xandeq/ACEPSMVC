11cb78b2d64a9b7cc273807167995db399183410
xandeq <xandeq@gmail.com>
2021-04-26 08:22:13 -0300

    Consertando os destaques para mostrar de 2 em 2 no topo da pagina principal

--- ACEPSMVC/Controllers/HomeController.cs
+++ ACEPSMVC/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace ACEPSMVC.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ContextoDBAplicacao _db;
-        
+
         public HomeController(ILogger<HomeController> logger, ContextoDBAplicacao db)
         {
             _logger = logger;
@@ -23,40 +23,48 @@ namespace ACEPSMVC.Controllers
 
         public IActionResult Index()
         {
-            //ViewData["Destaques"] = _db.Destaque.ToList();
-            ViewData["Noticias"] = _db.Noticias.Take(10).Select( s => new Noticias
+            try
             {
-                DataCriacao = s.DataCriacao,
-                Id = s.Id,
-                ImagemDestaque = s.ImagemDestaque,
-                ImagemDestaqueArquivo = s.ImagemDestaqueArquivo,
-                ImagemInterna = s.ImagemInterna,
-                LinhaFina = s.LinhaFina,
-                Subtitulo = s.Subtitulo,
-                Texto = Regex.Replace(s.Texto, "<.*?>", String.Empty),
-                Titulo = s.Titulo
-            }).ToList();
-            List<DestaquesUnidos> listaDestaques = _db.Destaque.Select(s => new DestaquesUnidos
+                //ViewData["Destaques"] = _db.Destaque.ToList();
+                ViewData["Noticias"] = _db.Noticias.Take(10).Select(s => new Noticias
+                {
+                    DataCriacao = s.DataCriacao,
+                    Id = s.Id,
+                    ImagemDestaque = s.ImagemDestaque,
+                    ImagemDestaqueArquivo = s.ImagemDestaqueArquivo,
+                    ImagemInterna = s.ImagemInterna,
+                    LinhaFina = s.LinhaFina,
+                    Subtitulo = s.Subtitulo,
+                    Texto = Regex.Replace(s.Texto, "<.*?>", String.Empty),
+                    Titulo = s.Titulo
+                }).ToList();
+                List<DestaquesUnidos> listaDestaques = _db.Destaque.Select(s => new DestaquesUnidos
+                {
+                    Id = s.Id,
+                    Imagem = s.Imagem,
+                    Conteudo = s.Conteudo,
+                    DataCriacao = DateTime.Today,
+                    Ativo = true,
+                    Url = s.Url
+                }).ToList();
+                List<DestaquesUnidos> listaDestaquesPrincipais = _db.DestaquePrincipal.Select(s => new DestaquesUnidos
+                {
+                    Id = s.Id,
+                    Imagem = s.Imagem,
+                    Conteudo = s.Conteudo,
+                    DataCriacao = DateTime.Today,
+                    Ativo = true,
+                    Url = string.Empty
+                }).ToList();
+                ViewData["Destaques"] = listaDestaques.Concat(listaDestaquesPrincipais).ToList();
+                //ViewData["DestaquePrincipal"] = _db.DestaquePrincipal.OrderBy(o => o.Id).ToList();
+                ViewData["DestaquesLaterais"] = _db.DestaqueLateral.OrderBy(o => o.Id).ToList();
+            }
+            catch (Exception ex)
             {
-                Id = s.Id,
-                Imagem = s.Imagem,
-                Conteudo = s.Conteudo,
-                DataCriacao = DateTime.Today,
-                Ativo = true,
-                Url = s.Url
-            }).ToList();
-            List<DestaquesUnidos> listaDestaquesPrincipais = _db.DestaquePrincipal.Select(s => new DestaquesUnidos
-            {
-                Id = s.Id,
-                Imagem = s.Imagem,
-                Conteudo = s.Conteudo,
-                DataCriacao = DateTime.Today,
-                Ativo = true,
-                Url = string.Empty
-            }).ToList();
-            ViewData["Destaques"] = listaDestaques.Concat(listaDestaquesPrincipais);
-            //ViewData["DestaquePrincipal"] = _db.DestaquePrincipal.OrderBy(o => o.Id).ToList();
-            ViewData["DestaquesLaterais"] = _db.DestaqueLateral.OrderBy(o => o.Id).ToList();
+                //
+            }
+
             return View();
         }
 
