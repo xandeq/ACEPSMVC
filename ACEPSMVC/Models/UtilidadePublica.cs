043b9c9d72cc200092156e30116c94cc2c32073f
xandeq <xandeq@gmail.com>
2021-05-03 09:40:47 -0300

    Criação da tabela Utilidade Publica

---
+++ ACEPSMVC/Models/UtilidadePublica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACEPSMVC.Models
+{
+    public class UtilidadePublica
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo éobrigatório")]
+        [MaxLength(250, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Este campo éobrigatório")]
+        [MaxLength(250, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(7, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Url { get; set; }
+
+        [Required(ErrorMessage = "Este campo éobrigatório")]
+        [MaxLength(250, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Imagem { get; set; }
+
+        [Required]
+        public string DataCriacao { get; set; }
+    }
+}
