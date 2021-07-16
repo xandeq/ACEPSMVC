88a7ba1f03a3e1c1ecaf98560036adbe814e78a2
alexandre-queiroz-take <alexandre.queiroz.tc@take.net>
2021-07-16 08:07:38 -0300

    Lista de serviços, controller serviços

---
+++ ACEPSMVC/Controllers/ServicosController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ACEPSMVC.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Utilidades;
+
+namespace ACEPSMVC.Controllers
+{
+    public class ServicosController : Controller
+    {
+        private readonly ContextoDBAplicacao _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        [BindProperty]
+        public Servicos Servicos { get; set; }
+
+        public ServicosController(ContextoDBAplicacao db, IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListaServicos()
+        {
+            List<Servicos> listaServicos = _db.Servicos.ToList();
+            return View(listaServicos);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Servicos.OrderByDescending(o => o.Id).ToListAsync() });
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Servicos = new Servicos();
+            if (id == null)
+            {
+                //create
+                return View(Servicos);
+            }
+            //update
+            Servicos = _db.Servicos.FirstOrDefault(u => u.Id == id);
+            if (Servicos == null)
+            {
+                return NotFound();
+            }
+            return View(Servicos);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            //if (ModelState.IsValid)
+            //{
+            try
+            {
+                string caminho = _webHostEnvironment.WebRootPath + "\\servicos";
+
+                var filePath = Path.GetTempFileName();
+                foreach (var formFile in Request.Form.Files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        string caminhoArquivo = Path.Combine(caminho, formFile.FileName);
+
+                        caminhoArquivo = Helpers.ObterCaminhoArquivo(caminhoArquivo);
+                        using (var inputStream = new FileStream(caminhoArquivo, FileMode.Create))
+                        {
+                            // read file to stream
+                            formFile.CopyToAsync(inputStream);
+
+
+                            // stream to byte array
+                            byte[] array = new byte[inputStream.Length];
+                            inputStream.Seek(0, SeekOrigin.Begin);
+                            inputStream.Read(array, 0, array.Length);
+                            // get file name
+                            string fName = formFile.FileName;
+                            Servicos.Logo = formFile.FileName;
+                        }
+                    }
+                }
+                Servicos.DataCriacao = DateTime.Now.ToString();
+
+                if (Servicos.Id == 0)
+                {
+                    //create
+                    _db.Servicos.Add(Servicos);
+                }
+                else
+                {
+                    _db.Servicos.Update(Servicos);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                string message = string.Format("<b>Message:</b> {0}<br /><br />", ex.Message);
+                message += string.Format("<b>StackTrace:</b> {0}<br /><br />", ex.StackTrace.Replace(Environment.NewLine, string.Empty));
+                //message += string.Format("<b>InnerException:</b> {0}<br /><br />", ex.InnerException.ToString().Replace(Environment.NewLine, string.Empty));
+                message += string.Format("<b>Source:</b> {0}<br /><br />", ex.Source.Replace(Environment.NewLine, string.Empty));
+                message += string.Format("<b>TargetSite:</b> {0}", ex.TargetSite.ToString().Replace(Environment.NewLine, string.Empty));
+                ViewData["ErrorMessage"] = "Erro : " + message;
+            }
+            //}
+            return View(Servicos);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var objBD = await _db.Servicos.FirstOrDefaultAsync(u => u.Id == id);
+            if (objBD == null)
+            {
+                return Json(new { success = false, message = "Erro ao deletar Servicos." });
+            }
+
+            _db.Servicos.Remove(objBD);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Servicos deletada com sucesso." });
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            Servicos destaque = _db.Servicos.FirstOrDefault(u => u.Id == id);
+            return View(destaque);
+        }
+    }
+}
---
+++ ACEPSMVC/wwwroot/js/servicos.js
@@ -0,0 +1,71 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/Servicos/GetAll/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            {
+                "data": "logo", "width": "20%", "render": function (data) {
+                    console.log('ENTROU NA IMAGEM');
+                    return '<img src="../servicos/' + data + '" />';
+                }
+            },
+            { "data": "nome", "width": "20%" },
+            { "data": "website", "width": "20%" },
+            { "data": "email", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/Servicos/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                            Editar
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                            onclick=Deletar('/Servicos/Delete?id='+${data})>
+                            Deletar
+                        </a>
+                        </div>`;
+                }, "width": "20%"
+            }
+        ],
+        "language": {
+            "emptyTable": "Nenhum registro encontrado"
+        },
+        "width": "100%"
+    });
+}
+
+function Deletar(url) {
+    swal({
+        title: "Você tem certeza?",
+        text: "Uma vez deletado, você não terá como recuperar o serviço.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDeletar) => {
+        if (willDeletar) {
+            $.ajax({
+                type: "Delete",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
